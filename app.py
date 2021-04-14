f94a3570af6cfe0d2918eb0699e252f3dcc4d55c
Matthew-Petersen04 <43018151+Matthew-Petersen04@users.noreply.github.com>
2021-04-14 15:22:04 -0400

    Add files via upload
    Final Changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import webbrowser as wb
+import subprocess
     
 
 def open_rstudio():
@@ -25,6 +26,9 @@ def open_tensorflow():
     
 def open_spark():
     wb.open_new('http://host.docker.internal:8889/')
+
+#def open_tableau():
+#    subrocess.call("docker", "exec", "-it", "course_project_tableau_1", "bash") 
 
 
 root = tk.Tk()
